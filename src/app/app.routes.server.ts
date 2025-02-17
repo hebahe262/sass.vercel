55fba8440af83a482af408f92f9bd960e7db81a0
heba55 <gogoalimohamed300@gmail.com>
2025-02-17 22:57:03 +0200

    routing

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+
+  {
+    path: "details/:id",
+    renderMode: RenderMode.Server
+  },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
